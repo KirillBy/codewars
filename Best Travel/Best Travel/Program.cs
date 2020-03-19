6d060d14f57e4b0e29402ea81dec19f549ad8595
Kiryl Balanovich <balanovichks@gmail.com>
2020-03-19 15:40:38 +0300

    Best Travel

---
+++ Best Travel/Best Travel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Best_Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ts = new List<int>() { 50, 55, 56, 57, 58 };
+            Console.WriteLine(SumOfK.chooseBestSum(163, 3, ts)); 
+        }
+    }
+
+
+public static class SumOfK
+    {
+        public static int? chooseBestSum(int t, int k, List<int> ls) =>
+          ls.Combinations(k)
+            .Select(c => (int?)c.Sum())
+            .Where(sum => sum <= t)
+            .DefaultIfEmpty()
+            .Max();
+
+        public static IEnumerable<IEnumerable<int>> Combinations(this IEnumerable<int> ls, int k) =>
+          k == 0 ? new[] { new int[0] } :
+            ls.SelectMany((e, i) =>
+              ls.Skip(i + 1)
+                .Combinations(k - 1)
+                .Select(c => (new[] { e }).Concat(c)));
+    }
+}
