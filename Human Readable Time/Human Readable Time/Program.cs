f00667dfcf248822c0be540837456184db92d765
Kiryl Balanovich <balanovichks@gmail.com>
2020-03-20 12:12:20 +0300

    Human Readable Time

---
+++ Human Readable Time/Human Readable Time/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Human_Readable_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeFormat.GetReadableTime(61)); 
+        }
+    }
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int hour = seconds / 3600;
+            int minute = (seconds % 3600) / 60;
+            int sec = seconds - hour * 3600 - minute * 60;
+            return TimeToStr(hour)+":"+TimeToStr(minute) + ":" + TimeToStr(sec);
+        }
+        private static string TimeToStr(int time)
+        {
+            if (time < 10)
+                return "0" + time.ToString();
+            else
+                return time.ToString();
+        }
+    }
+}
